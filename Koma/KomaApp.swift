4fa1b9c824c1a5245a2ee9e7bb44ca53ad78d815
antonio93hb <antonio93hb@hotmail.com>
2025-06-20 13:22:28 +0200

    Initial Commit

---
+++ Koma/KomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomaApp.swift
+//  Koma
+//
+//  Created by Antonio Hernández Barbadilla on 20/6/25.
+//
+
+import SwiftUI
+
+@main
+struct KomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
